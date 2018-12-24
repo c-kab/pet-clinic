4add4b6c32fa73422b25dfbe6271ae57d33fa3f8
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2018-12-24 22:45:43 +0100

    Added Spring Confi . closes #22

--- pet-clinic-data/src/main/java/com/example/petclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.example.petclinic.service.map;
 
 import com.example.petclinic.model.Owner;
 import com.example.petclinic.service.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
--- pet-clinic-data/src/main/java/com/example/petclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/map/VetServiceMap.java
@@ -2,9 +2,12 @@ package com.example.petclinic.service.map;
 
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.service.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
