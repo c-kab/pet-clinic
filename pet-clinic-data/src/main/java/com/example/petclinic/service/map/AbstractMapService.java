9e25f8a3b74dcc0d3ab40bc517fea80c592e3b58
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2018-12-25 06:54:38 +0100

    ID property refactored to increment automatically. closes #23

--- pet-clinic-data/src/main/java/com/example/petclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.example.petclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.example.petclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>() ;
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>() ;
 
     Set<T> findAll(){
 
@@ -21,7 +20,18 @@ public abstract class AbstractMapService<T, ID> {
 
     T save(ID id, T object){
 
-        map.put(id, object) ;
+        if(object != null ){
+            if(object.getId() == null ){
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object) ;
+
+        }else{
+            throw new RuntimeException("Object should be set to null") ;
+        }
+
+       // map.put(id, object) ;
         return object ;
     }
 
@@ -30,6 +40,22 @@ public abstract class AbstractMapService<T, ID> {
     }
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object)) ;
+    }
+
+
+    private Long getNextId(){
+        Long nextId = null ;
+
+        try {
+            nextId = Collections.max(map.keySet()) + 1 ;
+        } catch (NoSuchElementException e ){
+            nextId = 1L ;
+        }
+
+
+
+
+        return nextId ;
     }
 
 
