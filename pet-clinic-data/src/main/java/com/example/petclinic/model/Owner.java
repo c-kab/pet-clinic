97a03c93027c50782febc078ab248e69a406626f
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2019-01-02 14:45:38 +0100

    Added contact info properties to owner. closes #28

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Owner.java
@@ -4,7 +4,8 @@ import java.util.Set;
 
 public class Owner extends  Person  {
 
-    Set<Pet> pets  ;
+    private Set<Pet> pets  ;
+    private String address ;
 
     public Set<Pet> getPets() {
         return pets;
@@ -13,4 +14,33 @@ public class Owner extends  Person  {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    private String city ;
+    private String telephone ;
+
+
 }
