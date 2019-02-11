5a4f705207eb6b38e7a8d3ec0d52b25b2e4f1966
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2019-02-11 19:08:20 +0100

    JPA mapping for visit  class. closes #38

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.example.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -9,6 +11,18 @@ public class Pet  extends  BaseEntity{
 
     @Column(name ="name")
     private String name ;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     @ManyToOne
     @JoinColumn(name = "type_id")
--- pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
@@ -1,11 +1,21 @@
 package com.example.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends  BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date ;
+
+    @Column(name = "description")
     private  String description ;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private  Pet pet ;
 
     public LocalDate getDate() {
         return date;
@@ -31,5 +41,5 @@ public class Visit extends  BaseEntity {
         this.pet = pet;
     }
 
-    private  Pet pet ;
+
 }
