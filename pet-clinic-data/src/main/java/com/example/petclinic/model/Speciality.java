6faed68cbb2e83e02939115d2576d31edab61f82
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2019-02-11 17:47:49 +0100

    JPA mappings for vet and specialty  classes. closes #37

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
@@ -1,10 +1,16 @@
 package com.example.petclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description ;
-
-
     public String getDescription() {
         return description;
     }
