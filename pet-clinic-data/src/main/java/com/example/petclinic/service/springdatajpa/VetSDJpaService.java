23ec73d0864d8b7d6c5d5d38d1c7d910635e1a9e
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2019-02-12 14:04:27 +0100

    Spring data jpa service for pets  added. closes #43

--- pet-clinic-data/src/main/java/com/example/petclinic/service/springdatajpa/VetSDJpaService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/service/springdatajpa/VetSDJpaService.java
@@ -3,10 +3,14 @@ package com.example.petclinic.service.springdatajpa;
 import com.example.petclinic.model.Vet;
 import com.example.petclinic.repositories.VetRepository;
 import com.example.petclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
+@Profile("springdatajpa")
 public class VetSDJpaService implements VetService {
 
     private final VetRepository vetRepository ;
