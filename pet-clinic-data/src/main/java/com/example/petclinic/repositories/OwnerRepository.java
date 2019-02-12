39ca1a6f2d3379b24e74337e850c867354055d61
cicekapM <ciceron.kapena-bantu-muetu@grenoble-inp.org>
2019-02-12 13:12:48 +0100

    Spring data jpa service for owners added. closes #40

--- pet-clinic-data/src/main/java/com/example/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/repositories/OwnerRepository.java
@@ -3,5 +3,8 @@ package com.example.petclinic.repositories;
 import com.example.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
